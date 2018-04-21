39bad600a2b77b439900385e58c4c4fe4657935e
Davide Sozzi <davide.sozzi@eidosmedia.com>
2018-04-21 13:45:59 +0200

    Implemented relative paths

--- README.md
+++ README.md
@@ -18,6 +18,10 @@ npm start
 
 and visit `localhost:3000`
 
+### Demo
+
+[http://www.musaethemes.com/spotify-react](http://www.musaethemes.com/spotify-react)
+
 ### Sidenotes
 
 This is an experiment, only few sections are currentl visibles and many things are hardcoded.
--- src/App.js
+++ src/App.js
@@ -5,133 +5,144 @@ import Home from "./views/home/Home";
 import Radio from "./views/radio/Radio";
 import { BrowserRouter as Router, Route, NavLink, Switch } from "react-router-dom";
 
+const Root = ({ match }) => {
+    if (match.url.endsWith("/")) {
+        match.url = match.url.replace(/\/$/, "");
+    }
+    return (
+        <div className="App">
+            {/* create header compnent */}
+            <div className="App-header">
+                <div className="window-controls">
+                    <span className="window-control--close" />
+                    <span className="window-control--minimize" />
+                    <span className="window-control--maximize" />
+                </div>
+            </div>
+            {/* create sidebar compnent */}
+            <div id="main">
+                <div className="sidebar">
+                    <ul>
+                        <li>
+                            {console.log(match)}
+                            <NavLink exact to={`${match.url}`} activeClassName="active">
+                                Browse
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/radio`} activeClassName="active">
+                                Radio
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <span className="sidebar__title">YOUR LIBRARY</span>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/daily`} activeClassName="active">
+                                Your Daily Mix
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/recents`} activeClassName="active">
+                                Recently Played
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/songs`} activeClassName="active">
+                                Songs
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/albums`} activeClassName="active">
+                                Albums
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/artists`} activeClassName="active">
+                                Artists
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/stations`} activeClassName="active">
+                                Stations
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/local-files`} activeClassName="active">
+                                Local Files
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/podcasts`} activeClassName="active">
+                                Podcasts
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <span className="sidebar__title">PLAYLISTS</span>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/kids`} activeClassName="active">
+                                Bambini che ballano
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/battisti`} activeClassName="active">
+                                Lucio Battisti
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/mazinga`} activeClassName="active">
+                                Mazinga Z
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/rocky`} activeClassName="active">
+                                Rocky Balboa
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/baby`} activeClassName="active">
+                                Baby Dance
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/celentano`} activeClassName="active">
+                                Celentano
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={`${match.url}/minogue`} activeClassName="active">
+                                Kylie Minogue
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <div id="mainArea">
+                    {/* <section className="main-header">
+                            <div className="main-header__background"></div>
+                        </section> */}
+                    <Switch>
+                        {console.log(match.url)}
+                        <Route exact path={`${match.url}/`} component={Home} />
+                        <Route path={`${match.url}/daily`} component={Daily} />
+                        <Route path={`${match.url}/radio`} component={Radio} />
+                    </Switch>
+                </div>
+            </div>
+            <div id="player" />
+        </div>
+    );
+};
+
 class App extends Component {
     render() {
         return (
             <Router>
-                <div className="App">
-                    {/* create header compnent */}
-                    <div className="App-header">
-                        <div className="window-controls">
-                            <span className="window-control--close" />
-                            <span className="window-control--minimize" />
-                            <span className="window-control--maximize" />
-                        </div>
-                    </div>
-                    {/* create sidebar compnent */}
-                    <div id="main">
-                        <div className="sidebar">
-                            <ul>
-                                <li>
-                                    <NavLink exact to="/" activeClassName="active">
-                                        Browse
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/radio" activeClassName="active">
-                                        Radio
-                                    </NavLink>
-                                </li>
-                            </ul>
-                            <ul>
-                                <li>
-                                    <span className="sidebar__title">YOUR LIBRARY</span>
-                                </li>
-                                <li>
-                                    <NavLink to="/daily" activeClassName="active">
-                                        Your Daily Mix
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/recents" activeClassName="active">
-                                        Recently Played
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/songs" activeClassName="active">
-                                        Songs
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/albums" activeClassName="active">
-                                        Albums
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/artists" activeClassName="active">
-                                        Artists
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/stations" activeClassName="active">
-                                        Stations
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/local-files" activeClassName="active">
-                                        Local Files
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/podcasts" activeClassName="active">
-                                        Podcasts
-                                    </NavLink>
-                                </li>
-                            </ul>
-                            <ul>
-                                <li>
-                                    <span className="sidebar__title">PLAYLISTS</span>
-                                </li>
-                                <li>
-                                    <NavLink to="/bambini-che-ballano" activeClassName="active">
-                                        Bambini che ballano
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/battisti" activeClassName="active">
-                                        Lucio Battisti
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/mazinga" activeClassName="active">
-                                        Mazinga Z
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/rocky" activeClassName="active">
-                                        Rocky Balboa
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dance" activeClassName="active">
-                                        Baby Dance
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/celentano" activeClassName="active">
-                                        Celentano
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/minogue" activeClassName="active">
-                                        Kylie Minogue
-                                    </NavLink>
-                                </li>
-                            </ul>
-                        </div>
-                        <div id="mainArea">
-                            {/* <section className="main-header">
-                            <div className="main-header__background"></div>
-                        </section> */}
-                            <Switch>
-                                <Route exact path="/" component={Home} />
-                                <Route exact path="/daily" component={Daily} />
-                                <Route path="/radio" component={Radio} />
-                            </Switch>
-                        </div>
-                    </div>
-                    <div id="player" />
-                </div>
+                <Route path="/spotify-react" component={Root} />
             </Router>
         );
     }
